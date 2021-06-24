cd2a3aa98909cbc25d3b59886557cea4b3d2e139
Spencer <spenclark@hotmail.com>
2021-06-24 10:13:46 -0700

    return err

--- api/server.js
+++ api/server.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const middleware = require('./middleware/server.middleware');
-const routes = require('./routes/routes.index');
+const express = require("express");
+const middleware = require("./middleware/server.middleware");
+const routes = require("./routes/routes.index");
 
 const server = express();
 middleware(server);
 routes(server);
 
 server.use((error, req, res, next) => {
-  new Promise((resolve, reject) => {
-    resolve();
-    reject(error);
-  }).catch(error => {
-    console.log(error);
-  });
+  if (error && error.name && error.name === "UnauthorizedError") {
+    return res.status(error.status).json({ message: error.message });
+  }
+  // console.log('Req: %j', JSON.stringify(req))
+  // console.log('Missed Error Handling Opportunity: %j', error);
+  res.status(500).json({ message: "An Error Has Occurred", error });
 });
 
 module.exports = server;
