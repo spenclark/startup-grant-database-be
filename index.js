bbe1c8ae85bd432e57177c3adeb518a63b7ace50
Spencer <spenclark@hotmail.com>
2021-06-24 10:09:37 -0700

    removed listener

--- index.js
+++ index.js
@@ -4,12 +4,4 @@ const server = require("./api/server.js");
 
 const port = process.env.PORT || 5000;
 
-server.listen(
-  port,
-  () =>
-    process.on("unhandledRejection", (reason, promise) => {
-      Promise.reject(new Error("This is fine"));
-    }),
-
-  console.log(`\n** Running on port ${port} **\n`)
-);
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
