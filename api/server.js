28295e50d74ca80e04b306929eda07a287e99db9
Spencer <spenclark@hotmail.com>
2021-06-24 09:54:19 -0700

    added warn flag

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const middleware = require("./middleware/server.middleware");
-const routes = require("./routes/routes.index");
+const express = require('express');
+const middleware = require('./middleware/server.middleware');
+const routes = require('./routes/routes.index');
 
 const server = express();
 middleware(server);
