72fb06702e118b693dd3d339f4548f48d1973672
Spencer <spenclark@hotmail.com>
2021-06-24 09:38:16 -0700

    testing new aysnc err catch

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const middleware = require('./middleware/server.middleware');
-const routes = require('./routes/routes.index');
+const express = require("express");
+const middleware = require("./middleware/server.middleware");
+const routes = require("./routes/routes.index");
 
 const server = express();
 middleware(server);
@@ -11,12 +11,16 @@ routes(server);
  * proper error handling on the routes.
  */
 server.use((error, req, res, next) => {
-  if (error && error.name && error.name === 'UnauthorizedError') {
-    return res.status(error.status).json({ message: error.message });
-  }
-  // console.log('Req: %j', JSON.stringify(req))
-  // console.log('Missed Error Handling Opportunity: %j', error);
-  res.status(500).json({ message: 'An Error Has Occurred', error });
+  new Promise((resolve, reject) => {
+    if (error && error.name && error.name === "UnauthorizedError") {
+      return res.status(error.status).json({ message: error.message });
+    }
+    console.log("Req: %j", JSON.stringify(req));
+    console.log("Missed Error Handling Opportunity: %j", error);
+    res.status(500).json({ message: "An Error Has Occurred", error });
+  }).catch(error => {
+    next();
+  });
 });
 
 module.exports = server;
