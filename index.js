acec71d240a582ed2a0dba9b627bacbfb932747c
Spencer <spenclark@hotmail.com>
2021-06-24 09:58:07 -0700

    embedded listener on index

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const server = require('./api/server.js');
+const server = require("./api/server.js");
 
 const port = process.env.PORT || 5000;
+process.on("unhandledRejection", (reason, promise) => {
+  Promise.reject(new Error("This is fine"));
+});
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
