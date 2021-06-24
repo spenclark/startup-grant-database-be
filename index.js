3591e57e04e5a21182cd02bfd1f107e94071e811
Spencer <spenclark@hotmail.com>
2021-06-24 10:03:42 -0700

    restructured inside of listener

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ require("dotenv").config();
 const server = require("./api/server.js");
 
 const port = process.env.PORT || 5000;
-process.on("unhandledRejection", (reason, promise) => {
-  Promise.reject(new Error("This is fine"));
-});
-server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+server.listen(
+  port,
+  () =>
+    process.on("unhandledRejection", (reason, promise) => {
+      Promise.reject(new Error("This is fine"));
+    }),
+
+  console.log(`\n** Running on port ${port} **\n`)
+);
